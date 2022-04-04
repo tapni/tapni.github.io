3e6c3a23ee6a03a7664574acdf2e7658ab1c9eac
tapni <85814056+tapni@users.noreply.github.com>
2022-04-05 03:02:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+<script type="text/javascript">
+    window.onload = function() {
+        var my_audio = new Audio("file:///C:/Users/tapni/Downloads/windows_8_usb_sound.mp3");
+            //ボタンにクリックイベントを設定
+            document.getElementById("btn20200502").onclick = function() {
+                my_audio.currentTime = 0;  //再生開始位置を先頭に戻す
+                my_audio.play();  //サウンドを再生
+            }
+    }
+</script>
