2ff8624ab0f336adf62c99431b93951830960397
tapni <85814056+tapni@users.noreply.github.com>
2022-07-29 21:20:11 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+window.addEventListener("scroll", function () {
+    const topBtn = document.getElementById("topbutton");
+    const scroll = window.pageYOffset;
+    if (scroll > 1000) {      // ➃
+      topBtn.style.opacity = 0.5;
+    } else topBtn.style.opacity = 0; 
+  });
